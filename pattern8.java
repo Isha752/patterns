446fe5c7a504a1bee334ce185ff617d22e047705
Isha752 <140087638+Isha752@users.noreply.github.com>
2024-05-13 09:12:44 +0530

    Create pattern8.java
    1 
    2 3 
    4 5 6 
    7 8 9 10 
    11 12 13 14 15

---
+++ pattern8.java
@@ -0,0 +1,10 @@
+ int a = 1;
+        System.out.println("Printing Pattern 8: ");
+        for(int i = 1; i<=5;i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(a + " ");
+                a++;
+            }
+            System.out.println();
+        }
+
