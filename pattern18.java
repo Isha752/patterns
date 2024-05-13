160d85f7a20785d995f3cfc02f04c63eb88b7f28
Isha752 <140087638+Isha752@users.noreply.github.com>
2024-05-13 09:22:18 +0530

    Create pattern18.java
    1111
    222
    33
    4

---
+++ pattern18.java
@@ -0,0 +1,10 @@
+System.out.println("Printing pattern 19: ");
+        int n = 4; 
+        int number=1;
+        for(int i = n; i>=1; i--){
+            for(int j = 1; j<=i;j++){
+                System.out.print(number);
+            }
+            number ++;
+            System.out.println();
+        }
