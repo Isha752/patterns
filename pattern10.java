d7466f64b23c8e8ed6bccb25416a44da6a60520b
Isha752 <140087638+Isha752@users.noreply.github.com>
2024-05-13 09:14:09 +0530

    Create pattern10.java
        *****
       *****
      *****
     *****
    *****

---
+++ pattern10.java
@@ -0,0 +1,10 @@
+ System.out.println("Printing Pattern 10: ");
+        for(int i = 1; i<=5; i++){
+            for(int j = 1; j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int z = 1; z<=5; z++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
