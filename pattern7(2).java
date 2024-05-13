7b67248d4595ce14ebea5a7809a9daef27d79d68
Isha752 <140087638+Isha752@users.noreply.github.com>
2024-05-13 09:11:58 +0530

    Create pattern7(2).java

---
+++ pattern7(2).java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class pattern2 {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n-i+1; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+*/ --> OUTPUT
+
+1234
+123
+12
+1
+
+  */
